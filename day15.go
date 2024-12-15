48c3c2c3d19008270e2961875f96c06d13c3cfed
fromenjn <fromenjn@skysoft-atm.com>
2024-12-15 17:30:37 +0100

    dirty but works

--- day15.go
+++ day15.go
@@ -35,39 +35,81 @@ func MakeRDFromChar(c rune) RD {
 	return RD{X: 0, Y: 0}
 }
 
-func (r *RP) Move(m [][]rune, rd RD) bool {
-	curChar := m[r.X][r.Y]
+func (r *RP) CanMove(m [][]rune, rd RD) bool {
 	nextPos := RP{X: r.X + rd.X, Y: r.Y + rd.Y}
 	res1 := true
 	res2 := true
+	//fmt.Printf("CanMove: curPos %c(%d,%d) / %c (%d,%d)\n", m[r.X][r.Y], r.X, r.Y, m[nextPos.X][nextPos.Y], nextPos.X, nextPos.Y)
+	if m[nextPos.X][nextPos.Y] == '#' {
+		return false
+	} else if m[nextPos.X][nextPos.Y] == '.' {
+		return true
+	} else if m[nextPos.X][nextPos.Y] == 'O' {
+		return nextPos.CanMove(m, rd)
+	} else if m[nextPos.X][nextPos.Y] == '[' {
+		otherHalf := RP{X: nextPos.X, Y: nextPos.Y + 1}
+		res1 = nextPos.CanMove(m, rd)
+		if res1 && rd.Y == 0 {
+			res2 = otherHalf.CanMove(m, rd)
+		}
+		return res1 && res2
+	} else if m[nextPos.X][nextPos.Y] == ']' {
+		otherHalf := RP{X: nextPos.X, Y: nextPos.Y - 1}
+		res1 = nextPos.CanMove(m, rd)
+		if res1 && rd.Y == 0 {
+			res2 = otherHalf.CanMove(m, rd)
+		}
+		return res1 && res2
+	}
+
+	return res1 && res2
+}
+
+func (r *RP) Move(m [][]rune, rd RD) bool {
+	curChar := m[r.X][r.Y]
+	nextPos := RP{X: r.X + rd.X, Y: r.Y + rd.Y}
 
 	if m[nextPos.X][nextPos.Y] == '#' {
 		return false
 	} else if m[nextPos.X][nextPos.Y] == '.' {
-		res1 = true
-	} else if m[nextPos.X][nextPos.Y] == 'O' {
-		res1 = nextPos.Move(m, rd)
-	} else if m[nextPos.X][nextPos.Y] == '[' {
-		otherHalf := RP{X: nextPos.X, Y: nextPos.Y + 1}
-		res1 = nextPos.Move(m, rd)
-		if res1 && rd.Y != 1 {
-			res2 = otherHalf.Move(m, rd)
-		}
-	} else if m[nextPos.X][nextPos.Y] == ']' {
-		otherHalf := RP{X: nextPos.X, Y: nextPos.Y - 1}
-		res1 = nextPos.Move(m, rd)
-		if res1 && rd.Y != -1 {
-			res2 = otherHalf.Move(m, rd)
-		}
-	}
-
-	if res1 && res2 {
 		m[r.X][r.Y] = '.'
 		m[nextPos.X][nextPos.Y] = curChar
 		return true
+	} else if m[nextPos.X][nextPos.Y] == 'O' {
+		res := nextPos.Move(m, rd)
+		if res {
+			m[r.X][r.Y] = '.'
+			m[nextPos.X][nextPos.Y] = curChar
+		}
+		return res
+	} else if m[nextPos.X][nextPos.Y] == '[' {
+		otherHalf := RP{X: nextPos.X, Y: nextPos.Y + 1}
+		res1 := nextPos.Move(m, rd)
+		res2 := true
+		if rd.Y != 1 {
+			res2 = otherHalf.Move(m, rd)
+		}
+		if res1 && res2 {
+			m[r.X][r.Y] = '.'
+			m[nextPos.X][nextPos.Y] = curChar
+			return true
+		}
+		return false
+	} else if m[nextPos.X][nextPos.Y] == ']' {
+		otherHalf := RP{X: nextPos.X, Y: nextPos.Y - 1}
+		res1 := nextPos.Move(m, rd)
+		res2 := true
+		if rd.Y != -1 {
+			res2 = otherHalf.Move(m, rd)
+		}
+		if res1 && res2 {
+			m[r.X][r.Y] = '.'
+			m[nextPos.X][nextPos.Y] = curChar
+			return true
+		}
+		return false
 	}
-
-	//fmt.Printf("Error CanMove: %c (%d,%d)\n", m[nextPos.X][nextPos.Y], nextPos.X, nextPos.Y)
+	fmt.Printf("Error CanMove: %c (%d,%d)\n", m[nextPos.X][nextPos.Y], nextPos.X, nextPos.Y)
 	return false
 }
 
@@ -121,7 +163,7 @@ func MakeDoubleMap(lines []string) []string {
 }
 
 func main() {
-	filePath := "inputs/day15-sample3.txt"
+	filePath := "inputs/day15.txt"
 
 	lines, err := utils.ReadFile(filePath)
 	lm := make([]string, 0)
@@ -148,14 +190,16 @@ func main() {
 	rp := GetRPos(m)
 	rp2 := GetRPos(m2)
 	for i := 0; i < len(ld); i++ {
-		if rp.Move(m, ld[i]) {
+		if rp.CanMove(m, ld[i]) {
+			rp.Move(m, ld[i])
 			rp = RP{X: rp.X + ld[i].X, Y: rp.Y + ld[i].Y}
 		}
-		if rp2.Move(m2, ld[i]) {
+		if rp2.CanMove(m2, ld[i]) {
+			rp2.Move(m2, ld[i])
 			rp2 = RP{X: rp2.X + ld[i].X, Y: rp2.Y + ld[i].Y}
 		}
-		utils.PrintRune2DArray(m2)
-		fmt.Printf("------------\n")
+		//utils.PrintRune2DArray(m2)
+		//fmt.Printf("------------\n")
 	}
 	fmt.Println(CountScore(m))
 	fmt.Println(CountScore(m2))
